61cdf0918aee3cb6ad911cca2fbe80e2810d853d
Evandro Costa <evandro.souzac@gmail.com>
2023-09-22 22:26:08 -0300

    Add text color on `Layout` component

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,7 @@
 export default function Layout({ children }) {
   return (
-    <div className="max-w-max py-0 px-4 mt-12 mx-auto mb-24">{children}</div>
+    <div className="max-w-max py-0 px-4 mt-12 mx-auto mb-24 text-slate-100">
+      {children}
+    </div>
   );
 }
