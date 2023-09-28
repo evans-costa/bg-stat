1d2956d832421e1576bab8fba37b4b6922cddd02
Evandro Costa <evandro.souzac@gmail.com>
2023-09-28 09:34:45 -0300

    Remove api call and add props to the component

--- components/SearchBar/searchBar.js
+++ components/SearchBar/searchBar.js
@@ -1,33 +1,4 @@
-import { useState } from "react";
-
-export default function SearchBar() {
-  const [input, setInput] = useState("");
-
-  const getData = async (value) => {
-    console.log(value);
-    try {
-      const query = await fetch(
-        `https://www.boardgamegeek.com/xmlapi2/search?query=${value}`,
-      );
-      const response = await query.text();
-      let parser = new DOMParser();
-      let xml = parser.parseFromString(response, "text/xml");
-
-      const nameElements = xml.getElementsByTagName("name");
-
-      for (let i = 0; i < nameElements.length; i++) {
-        nameElements[i].getAttribute("value");
-      }
-    } catch (error) {
-      console.error("Erro ao buscar dados:", error);
-    }
-  };
-
-  const handleChange = (value) => {
-    setInput(value);
-    getData(value);
-  };
-
+export default function SearchBar({ input, onChange, onFocus }) {
   return (
     <div className="w-4/5 rounded-full h-10 px-4 mx-auto my-10 flex items-center bg-gray-600">
       <svg
@@ -46,7 +17,8 @@ export default function SearchBar() {
         className="placeholder:italic bg-transparent border-none h-full w-full text-lg ml-1 focus:outline-none"
         placeholder="Digite o nome do jogo..."
         value={input}
-        onChange={(e) => handleChange(e.target.value)}
+        onChange={onChange}
+        onFocus={onFocus}
       />
     </div>
   );
