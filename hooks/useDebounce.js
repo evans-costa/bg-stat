9f3caa9d92af2708099599d7b3d6be5818045b8b
Evandro Costa <evandro.souzac@gmail.com>
2023-09-29 00:07:32 -0300

    Remove unused nullish coalescing operator

--- hooks/useDebounce.js
+++ hooks/useDebounce.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
-export default function useDebounce({ value, delay }) {
-  const [debouncedValue, setDebouncedValue] = useState(value ?? "");
+export default function useDebounce({ value = "", delay }) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
     const id = setTimeout(() => setDebouncedValue(value), delay ?? 500);
