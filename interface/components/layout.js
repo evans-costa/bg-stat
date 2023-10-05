9d6638095d3b372ee707de0189a0dee958c1ce62
Evandro Costa <evandro.souzac@gmail.com>
2023-10-04 23:26:25 -0300

    Add `Header` component to `Layout` and minor adjusts on style

--- interface/components/layout.js
+++ interface/components/layout.js
@@ -1,6 +1,9 @@
+import Header from "./Header";
+
 export default function Layout({ children }) {
   return (
-    <div className="max-w-max py-0 px-4 mt-12 mx-auto mb-24 text-slate-100">
+    <div className="w-auto mx-auto mb-24 text-slate-100">
+      <Header />
       {children}
     </div>
   );
