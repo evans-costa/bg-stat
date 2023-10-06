f61d39ea1dc95c53b53eb0f01ff98268a81da9c5
Evandro Costa <evandro.souzac@gmail.com>
2023-10-05 23:47:44 -0300

    Add price to table row component

--- interface/components/TableRow/index.js
+++ interface/components/TableRow/index.js
@@ -1,14 +1,19 @@
-import BoardgameImage from "../BoardgameImage"
+import BoardgameImage from "../BoardgameImage";
 
 export default function TableRow({ rowsData }) {
   const tableRows = rowsData.map((data, index) => {
     return (
       <tr key={index} className="border-b dark:border-neutral-500">
         <td className="whitespace-nowrap px-6 py-4">
-          {data ? <BoardgameImage boardgame={data} size={80} /> : null}
+          {data.image ? <BoardgameImage boardgame={data} size={80} /> : null}
         </td>
-        {data ? (
+        {data.name ? (
           <td className="whitespace-nowrap px-6 py-4">{data.name}</td>
+        ) : null}
+        {data.price ? (
+          <td className="whitespace-nowrap px-6 py-4">
+            {data.currency} {data.price}
+          </td>
         ) : null}
       </tr>
     );
