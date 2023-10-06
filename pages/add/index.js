c039b8bff4007e95587eff350f4be20cc3ea2457
Evandro Costa <evandro.souzac@gmail.com>
2023-10-05 23:47:05 -0300

    Import fetch data logic from `utils` folder

--- pages/add/index.js
+++ pages/add/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import useDebounce from "../../interface/hooks/useDebounce.js";
 
+import { searchGameData } from "../../interface/utils/searchGameData.js";
+
 import Layout from "../../interface/components/layout.js";
 import SearchBar from "../../interface/components/SearchBar";
 import SearchList from "../../interface/components/SearchList";
@@ -32,42 +34,8 @@ export default function Add() {
       setError(null);
 
       try {
-        const query = await fetch(
-          `https://boardgamegeek.com/xmlapi2/search?query=${debouncedInput}&type=boardgame&exact=0`,
-          { signal: controller.signal },
-        );
-        const response = await query.text();
-
-        let parser = new DOMParser();
-        let xml = parser.parseFromString(response, "text/xml");
-
-        const itemElements = xml.getElementsByTagName("item");
-
-        if (itemElements.length === 0) {
-          setError("No results to be shown!");
-          return;
-        }
-
-        const filteredResults = Array.from(itemElements).map((itemElement) => {
-          const nameElement = itemElement.getElementsByTagName("name")[0];
-          const name = nameElement.getAttribute("value");
-          const id = itemElement.getAttribute("id");
-          const yearPublished =
-            itemElement.getElementsByTagName("yearpublished")[0];
-          const year = yearPublished
-            ? yearPublished.getAttribute("value")
-            : null;
-
-          const boardgameResult = {
-            name: name,
-            id: id,
-            year: year ?? null,
-          };
-
-          return boardgameResult;
-        });
-
-        setResults(filteredResults);
+        const searchResult = await searchGameData(debouncedInput, controller);
+        setResults(searchResult);
       } catch (error) {
         setError("An error ocurred while fetching data.");
         setResults([]);
