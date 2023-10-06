b79b874e39820bb399a5b6c791fa3ecd49069b86
Evandro Costa <evandro.souzac@gmail.com>
2023-10-05 23:48:17 -0300

    Add margin to component

--- interface/components/ErrorMessage/index.js
+++ interface/components/ErrorMessage/index.js
@@ -2,7 +2,7 @@ import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 export default function ErrorMessage({ message }) {
   return (
-    <div className="text-red-500 font-normal flex items-center gap-1 text-xl">
+    <div className="text-red-500 font-normal flex items-center gap-1 text-xl mb-4">
       <InformationCircleIcon className="h-6 w-6" /> {message}
     </div>
   );
