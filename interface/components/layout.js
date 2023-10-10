675dc79644195a13a4dc21be628fc3f7c192bde0
Evandro Costa <evandro.souzac@gmail.com>
2023-10-09 22:16:47 -0300

    Remove margin botton on global layout

--- interface/components/layout.js
+++ interface/components/layout.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
-    <div className="w-auto mx-auto mb-24 text-slate-100">
+    <div className="w-auto mx-auto text-slate-100">
       <Header />
       {children}
     </div>
