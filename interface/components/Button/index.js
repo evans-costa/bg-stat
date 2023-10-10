e03b4a7b6685a82f8e22f01c82dac5d8db50212f
Evandro Costa <evandro.souzac@gmail.com>
2023-10-09 22:19:17 -0300

    Minor change on background color

--- interface/components/Button/index.js
+++ interface/components/Button/index.js
@@ -1,11 +1,11 @@
 export default function Button({ onClick, icon }) {
   return (
-      <button
-        type="button"
-        className="rounded-full text-sm px-3 py-3 items-center bg-gray-700 text-white"
-        onClick={onClick}
-      >
-        {icon}
-      </button>
+    <button
+      type="button"
+      className="rounded-full text-sm px-3 py-3 items-center bg-gray-800 text-white"
+      onClick={onClick}
+    >
+      {icon}
+    </button>
   );
 }
