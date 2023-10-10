13dab0888110f946dad0d02516ee5e28d6918cd1
Evandro Costa <evandro.souzac@gmail.com>
2023-10-09 22:13:18 -0300

    Change page to a new folder

---
+++ pages/boardgames/index.js
@@ -0,0 +1,41 @@
+import data from "../../_data/boardgames.json";
+
+import Layout from "../../interface/components/layout";
+import TableRow from "../../interface/components/TableRow";
+
+export default function Boardgames({ allBoardgameData }) {
+  return (
+    <Layout>
+      <div className="w-full flex items-center justify-center mt-12">
+        <table className="table-auto min-w-4/5 text-left text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th scope="col" className="px-6 py-4">
+                Imagem (BGG Link)
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Nome do Jogo
+              </th>
+              <th scope="col" className="px-6 py-4">
+                Preço de aquisição
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableRow rowsData={allBoardgameData} />
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allBoardgameData = data.boardgames;
+
+  return {
+    props: {
+      allBoardgameData,
+    },
+  };
+}
