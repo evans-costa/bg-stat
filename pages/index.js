134b8ea0eec9bbd816ae0e41d575431b87cb2988
Evandro Costa <evandro.souzac@gmail.com>
2023-10-09 22:16:06 -0300

    Add login form on initial page

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,73 @@
-import Head from "next/head";
-
-import data from "../_data/boardgames.json";
-
 import Layout from "../interface/components/layout";
-import TableRow from "../interface/components/TableRow";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Layout>
-      <Head>
-        <title>BG Collection Stat</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <div className="w-full flex items-center justify-center mt-12">
-        <table className="table-auto min-w-4/5 text-left text-sm font-light">
-          <thead className="border-b font-medium dark:border-neutral-500">
-            <tr>
-              <th scope="col" className="px-6 py-4">
-                Imagem (BGG Link)
-              </th>
-              <th scope="col" className="px-6 py-4">
-                Nome do Jogo
-              </th>
-              <th scope="col" className="px-6 py-4">
-                Preço de aquisição
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <TableRow rowsData={data.boardgames} />
-          </tbody>
-        </table>
-      </div>
+      <section className="flex flex-col items-center justify-center mt-16 mx-auto">
+        <div className="flex items-center justify-center text-2xl text-slate-50 mb-6 font-semibold">
+          <p>Keep track of your boardgame collection</p>
+        </div>
+        <div className="w-full max-w-md bg-gray-900 border border-gray-700 rounded-lg">
+          <div className="py-6 px-8">
+            <div className="my-4">
+              <h1 className="text-slate-50 font-semibold text-2xl">
+                Sign in to your account
+              </h1>
+            </div>
+            <form className="space-y-4">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-slate-50 mb-2 text-sm"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-700 rounded-lg p-2.5 block border border-gray-600 text-slate-200"
+                  placeholder="name@email.com"
+                  required
+                  autoComplete="on"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-slate-50 mb-2 text-sm"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full bg-gray-700 rounded-lg p-2.5 block border border-gray-600 text-slate-200"
+                  placeholder="••••••••"
+                  required
+                  autoComplete="on"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-between">
+                <button
+                  type="submit"
+                  className="w-full my-4 rounded-lg p-2.5 bg-blue-700 text-base font-medium text-slate-50"
+                >
+                  Sign In
+                </button>
+                <p className="text-base text-gray-300">
+                  Don't have an account yet?{" "}
+                  <Link className="text-blue-500 font-semibold" href="/signup">
+                    Sign up
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
