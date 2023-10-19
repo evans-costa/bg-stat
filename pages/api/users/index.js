e389e9b700148043795b22e0ac64189855846688
Evandro Costa <evandro.souzac@gmail.com>
2023-10-18 22:52:46 -0300

    Modify catch error to use custom error message

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,22 +1,23 @@
-import * as user from "../../../models/user.js";
+import { AppError } from "../../../errors/index.js";
+import * as User from "../../../models/user.js";
 
 export default async function postHandler(request, response) {
   if (request.method === "POST") {
     try {
       const data = request.body;
 
-      await user.create(data);
+      await User.create(data);
 
       return response
         .status(201)
         .json({ message: "User created successfully!" });
     } catch (error) {
-      console.error(error);
-      return response
-        .status(400)
-        .json({ message: "Error while creating user, please try again" });
+      if (error instanceof AppError) {
+        console.error(error);
+        return response.status(400).json({ message: error.message });
+      }
     }
   } else {
-    res.status(405).end();
+    response.status(405).end();
   }
 }
