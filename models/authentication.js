f09404dfcd0823ab908fe43dea8d078b829401f4
Evandro Costa <evandro.souzac@gmail.com>
2023-10-26 22:49:01 -0300

    Change method to validade token and pass it to session model

--- models/authentication.js
+++ models/authentication.js
@@ -1,5 +1,5 @@
 import { AppError } from "../errors";
-import session from "./sessions.js";
+import * as Session from "./sessions.js";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import bcryptjs from "bcryptjs";
@@ -41,38 +41,8 @@ function setAuthCookie(authToken, response) {
   ]);
 }
 
-export function findValidAccessCookie(request, next) {
-  const accessToken = request.headers["authorization"]?.split(" ") || [
-    " ",
-    " ",
-  ];
-
-  if (!accessToken) {
-    throw new AppError({
-      message: "There are no access token on cookies",
-      statusCode: 401,
-    });
-  }
-
-  try {
-    const payload = jwt.verify(accessToken, process.env.JWT_SECRET);
-    const userIdFromToken = typeof payload !== "string" && payload.id;
-
-    if (!userIdFromToken) {
-      throw new AppError({
-        message: "The token provided is invalid",
-        statusCode: 401,
-      });
-    }
-
-    return next();
-  } catch (error) {
-    return error;
-  }
-}
-
 export async function createSessionAndAccessToken(userId, response) {
-  const refreshToken = await session.createRefreshToken(userId);
+  const refreshToken = await Session.createRefreshToken(userId);
   createAccessToken(userId, response);
   return refreshToken;
 }
