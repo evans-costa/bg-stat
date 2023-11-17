313cdaa73b7ab969f2290121bdff5fab04a654ac
Evandro Costa <evandro.souzac@gmail.com>
2023-11-16 23:37:09 -0300

    Conditional rendering on header to show button only on login

--- interface/components/Header/index.js
+++ interface/components/Header/index.js
@@ -1,14 +1,22 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
+import ActionMenu from '../ActionMenu';
 
-import { PlusIcon } from "@heroicons/react/20/solid";
+import { PlusIcon } from '@heroicons/react/20/solid';
+
+import useUser from '../../hooks/useUser';
 
 export default function Header() {
+  const { user } = useUser();
+
   return (
     <header>
       <nav className="bg-gray-900 px-4 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto">
-          <Link className="flex items-center gap-2" href="/">
+          <Link
+            className="flex items-center gap-2"
+            href={user ? '/boardgames' : '/'}
+          >
             <Image
               className="w-auto h-auto"
               src="/static/headerIcon.png"
@@ -19,12 +27,17 @@ export default function Header() {
             />
             <span className="font-bold text-lg">BG Collection Stat</span>
           </Link>
-          <Link
-            className="rounded-full text-sm px-3 py-3 items-center bg-gray-700 text-white"
-            href="/add"
-          >
-            <PlusIcon className="h-6 w-6" />
-          </Link>
+          {user && (
+            <div className="flex items-center gap-4">
+              <Link
+                className="text-sm px-2 py-2 items-center bg-slate-700 text-slate-400 hover:text-slate-100 rounded-full"
+                href="/add"
+              >
+                <PlusIcon className="h-5 w-5" />
+              </Link>
+              <ActionMenu />
+            </div>
+          )}
         </div>
       </nav>
     </header>
