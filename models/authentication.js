fe92d4a1d1f533d82911a0f0aa0f0670ed4516df
Evandro Costa <evandro.souzac@gmail.com>
2023-11-16 23:31:29 -0300

    Remove `createAccessToken` method and put into token model

--- models/authentication.js
+++ models/authentication.js
@@ -1,9 +1,8 @@
-import { AppError } from "../errors";
-import * as Session from "./sessions.js";
-import jwt from "jsonwebtoken";
-import cookie from "cookie";
-import bcryptjs from "bcryptjs";
-import "dotenv/config";
+import { AppError } from '../errors';
+import * as Session from './sessions.js';
+import * as Token from './token.js';
+import bcryptjs from 'bcryptjs';
+import 'dotenv/config';
 
 export async function comparePassword(userPasswordInput, passwordHash) {
   const passwordMatches = await bcryptjs.compare(
@@ -13,36 +12,14 @@ export async function comparePassword(userPasswordInput, passwordHash) {
 
   if (!passwordMatches) {
     throw new AppError({
-      message: "The informed password does not match with user password",
+      message: 'The informed password does not match with user password',
       statusCode: 400,
     });
   }
 }
 
-export function createAccessToken(userId, response) {
-  const accessToken = jwt.sign(
-    {
-      id: userId,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" },
-  );
-
-  setAuthCookie(accessToken, response);
-}
-
-function setAuthCookie(authToken, response) {
-  response.setHeader("Set-Cookie", [
-    cookie.serialize("accessToken", authToken, {
-      httpOnly: true,
-      maxAge: 3600,
-      path: "/",
-    }),
-  ]);
-}
-
 export async function createSessionAndAccessToken(userId, response) {
   const refreshToken = await Session.createRefreshToken(userId);
-  createAccessToken(userId, response);
+  Token.createAccessToken(userId, response);
   return refreshToken;
 }
