5b83b6a3e08e685bf300119dd9ffa3ca12d28736
Evandro Costa <evandro.souzac@gmail.com>
2023-11-16 23:30:24 -0300

    Remove `findToken` method and put into Token model

--- models/sessions.js
+++ models/sessions.js
@@ -1,13 +1,11 @@
-import crypto from "node:crypto";
-import jwt from "jsonwebtoken";
-import * as database from "../infra/database.js";
-import { AppError } from "../errors/index.js";
-import { NextResponse } from "next/server";
+import crypto from 'node:crypto';
+import * as database from '../infra/database.js';
+import 'dotenv/config';
 
 const SESSION_EXPIRATION_IN_SECONDS = 60 * 60 * 24 * 30; // 30 days
 
 export async function createRefreshToken(userId) {
-  const sessionToken = crypto.randomBytes(48).toString("hex");
+  const sessionToken = crypto.randomBytes(48).toString('hex');
   const expiresAt = new Date(Date.now() + 1000 * SESSION_EXPIRATION_IN_SECONDS);
 
   const query = {
@@ -18,30 +16,4 @@ export async function createRefreshToken(userId) {
 
   const results = await database.query(query);
   return results.rows[0];
-}
-
-export function findTokenAndValidateOnSession(request) {
-  const accessToken = request.cookies?.accessToken;
-
-  if (!accessToken) {
-    return NextResponse.next(
-      new AppError({
-        message: "There are no token provided in this session",
-        statusCode: 401,
-      }),
-    );
-  }
-
-  const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
-
-  if (!decoded) {
-    return NextResponse.next(
-      new AppError({
-        message: "The token is invalid.",
-        statusCode: 401,
-      }),
-    );
-  }
-
-  return decoded;
 }
