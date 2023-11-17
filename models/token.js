624435a5d68e7f3959d4b7ca55d63a3b7c98baa0
Evandro Costa <evandro.souzac@gmail.com>
2023-11-16 23:16:06 -0300

    Create `token.js` model file

---
+++ models/token.js
@@ -0,0 +1,61 @@
+import { AppError } from '../errors';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+
+export function createAccessToken(userId, response) {
+  const accessToken = jwt.sign(
+    {
+      id: userId,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '10s' },
+  );
+
+  setAuthCookie(accessToken, response);
+}
+
+function setAuthCookie(authToken, response) {
+  response.setHeader('Set-Cookie', [
+    cookie.serialize('accessToken', authToken, {
+      httpOnly: true,
+      maxAge: 3600,
+      path: '/',
+    }),
+  ]);
+}
+
+export function getTokenByCookie(request) {
+  const getAccessToken = request.cookies?.accessToken;
+
+  if (!getAccessToken) {
+    throw new AppError({
+      message: 'There are no token provided in this session',
+      statusCode: 403,
+    });
+  }
+
+  return getAccessToken;
+}
+
+export async function verifyTokenOnCookies(token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err)
+        reject(new AppError({
+          message: 'The token is invalid',
+          statusCode: 403,
+        }));
+      } else {
+        console.log(decoded)
+        resolve(decoded)
+      }
+    })
+  });
+}
+
+// export async function findTokenAndValidateOnSession(request) {
+//   const accessToken = getTokenByCookie(request);
+//   const decodedToken = await verifyTokenOnCookies(accessToken);
+//   return decodedToken;
+// }
