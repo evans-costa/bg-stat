63ada40c148bcfe6bb2bf1c772ced0d8caec1aa5
Evandro Costa <evandro.souzac@gmail.com>
2023-11-16 23:28:25 -0300

    Put the session object into API response

--- pages/api/sessions/index.js
+++ pages/api/sessions/index.js
@@ -1,8 +1,8 @@
-import * as User from "../../../models/user.js";
-import * as Authentication from "../../../models/authentication.js";
+import * as User from '../../../models/user.js';
+import * as Authentication from '../../../models/authentication.js';
 
 export default async function postHandler(request, response) {
-  if (request.method === "POST") {
+  if (request.method === 'POST') {
     let userLogin;
 
     try {
@@ -14,14 +14,17 @@ export default async function postHandler(request, response) {
     } catch (error) {
       if (error) {
         return response.status(400).json({
-          message: "The data is incorrect, please enter your data again.",
+          message: 'The data is incorrect, please enter your data again.',
         });
       }
     }
 
-    await Authentication.createSessionAndAccessToken(userLogin.id, response);
+    const sessionObject = await Authentication.createSessionAndAccessToken(
+      userLogin.id,
+      response,
+    );
 
-    return response.status(201).json({ message: "Login successfully!" });
+    return response.status(201).json(sessionObject);
   } else {
     return response.status(405).end();
   }
