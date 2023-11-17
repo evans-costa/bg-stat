1bdfaab12a74867b1910b215a950ca98e6a37e11
Evandro Costa <evandro.souzac@gmail.com>
2023-11-16 23:35:18 -0300

    Refactor all `useUser` hook and remove console debug

--- interface/hooks/useUser.js
+++ interface/hooks/useUser.js
@@ -7,28 +7,27 @@ import {
   useEffect,
 } from 'react';
 
-const protectedRoutes = ['/', '/signup'];
-
 const UserContext = createContext({
   user: null,
   error: null,
-  authUser: () => {},
+  fetchUser: () => {},
 });
+
+const protectedRoutes = ['/', '/signup'];
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
   const router = useRouter();
-  console.log(router);
 
-  const authUser = useCallback(async () => {
+  const fetchUser = useCallback(async () => {
     try {
       const response = await fetch('/api/user', { method: 'GET' });
       const responseBody = await response.json();
 
       if (response.status === 200) {
         const fetchedUser = responseBody;
-        console.log(fetchedUser);
+
         const setUserObject = {
           id: responseBody.id,
           username: responseBody.username,
@@ -38,10 +37,14 @@ export function UserProvider({ children }) {
         setUser(fetchedUser);
         localStorage.setItem('user', JSON.stringify(setUserObject));
       }
-    } catch (error) {
-      console.log(error);
-      setError(error);
 
+      if (response.status >= 400) {
+        const error = new Error(responseBody.message);
+        error.status = response.status;
+        throw error;
+      }
+    } catch (error) {
+      setError(error);
       if (error.status >= 400) {
         setUser(null);
         localStorage.removeItem('user');
@@ -50,20 +53,21 @@ export function UserProvider({ children }) {
   }, [router]);
 
   useEffect(() => {
-    console.log('To aqui');
     if (!router || !protectedRoutes.includes(router.pathname)) return;
 
     (async () => {
+      if (!user?.id) await fetchUser();
+
       if (!user?.id || router.pathname !== '/') return;
 
-      await router.replace('/boardgames');
+      router.replace('/boardgames');
     })();
-  }, [user, router, authUser]);
+  }, [user, router, fetchUser]);
 
   const contextValue = {
     user,
     error,
-    authUser,
+    fetchUser,
   };
 
   return (
