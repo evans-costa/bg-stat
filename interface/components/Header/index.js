56c11ea877973c22b431b205d594ee46dc7b8e73
Evandro Costa <evandro.souzac@gmail.com>
2024-06-03 21:04:31 -0300

    Add Signout button

--- interface/components/Header/index.js
+++ interface/components/Header/index.js
@@ -1,22 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import ActionMenu from '../ActionMenu';
 
 import { PlusIcon } from '@heroicons/react/20/solid';
-
-import useUser from '../../hooks/useUser';
+import { signOutForm } from '../../../lib/actions';
 
 export default function Header() {
-  const { user } = useUser();
-
   return (
     <header>
       <nav className="bg-gray-900 px-4 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto">
-          <Link
-            className="flex items-center gap-2"
-            href={user ? '/boardgames' : '/'}
-          >
+          <Link className="flex items-center gap-2" href="/boardgames">
             <Image
               className="w-auto h-auto"
               src="/static/headerIcon.png"
@@ -27,17 +20,23 @@ export default function Header() {
             />
             <span className="font-bold text-lg">BG Collection Stat</span>
           </Link>
-          {user && (
-            <div className="flex items-center gap-4">
-              <Link
-                className="text-sm px-2 py-2 items-center bg-slate-700 text-slate-400 hover:text-slate-100 rounded-full"
-                href="/add"
-              >
-                <PlusIcon className="h-5 w-5" />
-              </Link>
-              <ActionMenu />
-            </div>
-          )}
+
+          <div className="flex items-center gap-4">
+            <Link
+              className="text-sm px-2 py-2 items-center bg-slate-700 text-slate-400 hover:text-slate-100 rounded-full"
+              href="/add"
+            >
+              <PlusIcon className="h-5 w-5" />
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <form action={signOutForm}>
+              <button>
+                <div>SignOut</div>
+              </button>
+            </form>
+          </div>
         </div>
       </nav>
     </header>
