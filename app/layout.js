b2bb2ea93ab5ec3770605319aa4720b9f962417e
Evandro Costa <evandro.souzac@gmail.com>
2024-06-04 22:49:12 -0300

    Define font type on application

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,14 @@
-import '../styles/globals.css';
+import '@/styles/globals.css';
+import localFont from 'next/font/local';
+
+const notoSans = localFont({
+  src: '../public/fonts/noto-sans.ttf',
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={notoSans.className}>
       <body>{children}</body>
     </html>
   );
