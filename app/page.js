315db1c81dde2c142fa18eebe86f4974243bc8d0
Evandro Costa <evandro.souzac@gmail.com>
2024-06-04 23:01:52 -0300

    Style login page to a better design

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 'use client';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import { useFormState, useFormStatus } from 'react-dom';
 
-import Link from 'next/link';
-import Layout from '../interface/components/layout.js';
-import LoadingSpin from '../interface/components/LoadingSpin/index.js';
-import ErrorMessage from '../interface/components/ErrorMessage/index.js';
-import { authenticate } from '../lib/actions.js';
+import { authenticate } from '@/lib/actions.js';
+
+import LoadingSpin from '@/interface/components/LoadingSpin/index.js';
+import ErrorMessage from '@/interface/components/ErrorMessage/index.js';
+import BGIcon from '@/public/static/headerIcon.png';
 
 export default function Home() {
   const { pending } = useFormStatus();
@@ -19,74 +21,85 @@ export default function Home() {
   };
 
   return (
-    <Layout>
-      <section className="flex flex-col items-center justify-center mt-16 mx-auto">
-        {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
-        <div className="flex items-center justify-center text-2xl text-slate-50 mb-6 font-semibold">
-          <p>Keep track of your boardgame collection</p>
+    <section className="w-full h-screen flex flex-row ">
+      <div className="bg-blue-700 basis-7/12 text-slate-50 flex flex-col items-center py-10">
+        <Image
+          src={BGIcon}
+          width={100}
+          height={100}
+          alt="Meeple icon"
+          className="self-start mx-10"
+        />
+        <div className="w-3/4 mt-32 flex flex-col gap-6">
+          <h1 className="text-5xl font-semibold">
+            Mantenha em dia sua coleção de Jogos de Tabuleiro
+          </h1>
+          <p className="w-1/2 font-light text-2xl text-pretty text-slate-300">
+            Veja toda sua coleção com informações de data e preço de aquisição e
+            venda.
+          </p>
         </div>
-        <div className="w-full max-w-md bg-gray-900 border border-gray-700 rounded-lg">
-          <div className="py-6 px-8">
-            <div className="my-4">
-              <h1 className="text-slate-50 font-semibold text-2xl">
-                Sign in to your account
-              </h1>
+      </div>
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+      <div className="basis-1/2 flex flex-col justify-center items-center">
+        <div className="mx-auto w-3/5">
+          <h2 className="text-slate-50 text-5xl font-medium self-start mb-14">
+            Entrar
+          </h2>
+          <form className="space-y-7" action={dispatch}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-slate-50 mb-2 text-base"
+              >
+                Seu email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-transparent rounded-xl p-3.5 block border border-zinc-800 text-slate-200"
+                placeholder="nome@email.com"
+                required
+                autoComplete="on"
+              />
             </div>
-            <form className="space-y-4" action={dispatch}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-slate-50 mb-2 text-sm"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full bg-gray-700 rounded-lg p-2.5 block border border-gray-600 text-slate-200"
-                  placeholder="name@email.com"
-                  required
-                  autoComplete="on"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-slate-50 mb-2 text-sm"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="w-full bg-gray-700 rounded-lg p-2.5 block border border-gray-600 text-slate-200"
-                  placeholder="••••••••"
-                  required
-                  autoComplete="on"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-between">
-                <button
-                  aria-disabled={pending}
-                  onClick={handleClick}
-                  type="submit"
-                  className="w-full my-4 rounded-lg p-2.5 bg-blue-700 text-base font-medium text-slate-50"
-                >
-                  {pending ? <LoadingSpin /> : 'Sign in'}
-                </button>
-                <p className="text-base text-gray-300">
-                  Don't have an account yet?{' '}
-                  <Link className="text-blue-500 font-semibold" href="/signup">
-                    Sign up
-                  </Link>
-                </p>
-              </div>
-            </form>
-          </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-slate-50 mb-2 text-base"
+              >
+                Senha
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full bg-transparent rounded-xl p-3.5 block border border-zinc-800 text-slate-200"
+                placeholder="••••••••"
+                required
+                autoComplete="on"
+              />
+            </div>
+            <div className="flex flex-col items-center justify-between">
+              <button
+                aria-disabled={pending}
+                onClick={handleClick}
+                type="submit"
+                className="w-full my-4 rounded-xl p-4 bg-blue-700 text-base font-medium text-slate-50"
+              >
+                {pending ? <LoadingSpin /> : 'Entrar'}
+              </button>
+              <p className="text-base self-start text-slate-400">
+                Não tem uma conta ainda?{' '}
+                <Link className="text-blue-500 font-semibold" href="/signup">
+                  Cadastrar
+                </Link>
+              </p>
+            </div>
+          </form>
         </div>
-      </section>
-    </Layout>
+      </div>
+    </section>
   );
 }
