dbc554738051cae5ae7d9df1b09690e3104f92cf
Evandro Costa <evandro.souzac@gmail.com>
2024-06-11 23:17:45 -0300

    Adds middleware to handle unauthorized users

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
 import NextAuth from 'next-auth';
 import { authConfig } from '@/auth.config';
 
-export default NextAuth(authConfig).auth;
+const nextAuth = NextAuth(authConfig);
+export default nextAuth.auth;
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
