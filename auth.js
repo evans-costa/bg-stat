6bd7c98509d60760b5ee0850b31d9c7cdcd613bd
Evandro Costa <evandro.souzac@gmail.com>
2024-06-11 23:16:35 -0300

    Parse schema and validate in auth flow

--- auth.js
+++ auth.js
@@ -5,7 +5,10 @@ import Credentials from 'next-auth/providers/credentials';
 import { comparePassword } from '@/models/authentication';
 import { findByEmail } from '@/models/user';
 
+import { signInSchema } from './lib/zod';
+
 import { InvalidLoginError } from '@/errors';
+import { ZodError } from 'zod';
 
 async function getUser(email) {
   try {
@@ -26,27 +29,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           label: 'email',
         },
         password: {
-          label: 'Password',
+          label: 'password',
           type: 'password',
         },
       },
       authorize: async (credentials) => {
-        const user = await getUser(credentials.email);
-
-        if (!user) return null;
-
         try {
-          const passwordMatch = await comparePassword(
-            credentials.password,
-            user.password,
-          );
+          const { password, email } =
+            await signInSchema.parseAsync(credentials);
+
+          const user = await getUser(email);
+
+          const passwordMatch = await comparePassword(password, user.password);
 
           if (passwordMatch) return user;
         } catch (error) {
+          if (error instanceof ZodError) {
+            return null;
+          }
           throw new InvalidLoginError();
         }
-
-        return null;
       },
     }),
   ],
