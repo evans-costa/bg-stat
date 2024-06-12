643172234735e15f8ff82c344f770952e42d646b
Evandro Costa <evandro.souzac@gmail.com>
2024-06-11 23:20:11 -0300

    Adds validation on sign in fields

--- app/page.js
+++ app/page.js
@@ -1,24 +1,28 @@
 'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
 import { useFormState, useFormStatus } from 'react-dom';
-
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signInSchema } from '@/lib/zod';
 import { authenticate } from '@/lib/actions.js';
 
-import LoadingSpin from '@/interface/components/LoadingSpin/index.js';
-import ErrorMessage from '@/interface/components/ErrorMessage/index.js';
+import LoadingSpin from '@/interface/components/LoadingSpin';
+import ErrorMessage from '@/interface/components/ErrorMessage';
 import BGIcon from '@/public/static/headerIcon.png';
 
 export default function Home() {
-  const { pending } = useFormStatus();
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
-  const handleClick = (event) => {
-    if (pending) {
-      event.preventDefault();
-    }
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signInSchema),
+  });
 
   return (
     <section className="w-full h-screen flex flex-row ">
@@ -28,7 +32,7 @@ export default function Home() {
           width={100}
           height={100}
           alt="Meeple icon"
-          className="self-start mx-10"
+          className="self-start mx-10 w-auto h-auto"
         />
         <div className="w-3/4 mt-32 flex flex-col gap-6">
           <h1 className="text-5xl font-semibold">
@@ -40,13 +44,13 @@ export default function Home() {
           </p>
         </div>
       </div>
-      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
       <div className="basis-1/2 flex flex-col justify-center items-center">
         <div className="mx-auto w-3/5">
           <h2 className="text-slate-50 text-5xl font-medium self-start mb-14">
             Entrar
           </h2>
-          <form className="space-y-7" action={dispatch}>
+          {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+          <form className="space-y-7" action={handleSubmit(dispatch)}>
             <div>
               <label
                 htmlFor="email"
@@ -60,9 +64,10 @@ export default function Home() {
                 name="email"
                 className="w-full bg-transparent rounded-xl p-3.5 block border border-zinc-800 text-slate-200"
                 placeholder="nome@email.com"
-                required
                 autoComplete="on"
+                {...register('email')}
               />
+              <p className="text-red-600">{errors.email?.message}</p>
             </div>
             <div>
               <label
@@ -77,29 +82,37 @@ export default function Home() {
                 name="password"
                 className="w-full bg-transparent rounded-xl p-3.5 block border border-zinc-800 text-slate-200"
                 placeholder="••••••••"
-                required
                 autoComplete="on"
+                {...register('password')}
               />
+              <p className="text-red-600">{errors.password?.message}</p>
             </div>
-            <div className="flex flex-col items-center justify-between">
-              <button
-                aria-disabled={pending}
-                onClick={handleClick}
-                type="submit"
-                className="w-full my-4 rounded-xl p-4 bg-blue-700 text-base font-medium text-slate-50"
-              >
-                {pending ? <LoadingSpin /> : 'Entrar'}
-              </button>
-              <p className="text-base self-start text-slate-400">
-                Não tem uma conta ainda?{' '}
-                <Link className="text-blue-500 font-semibold" href="/signup">
-                  Cadastrar
-                </Link>
-              </p>
-            </div>
+            <SubmitButton />
+            <p className="text-base self-start text-slate-400">
+              Não tem uma conta ainda?{' '}
+              <Link className="text-blue-500 font-semibold" href="/signup">
+                Cadastrar
+              </Link>
+            </p>
           </form>
         </div>
       </div>
     </section>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <button
+        type="submit"
+        disabled={pending}
+        className="w-full my-4 rounded-xl p-4 bg-blue-700 text-base font-medium text-slate-50"
+      >
+        {pending ? <LoadingSpin /> : 'Entrar'}
+      </button>
+    </div>
   );
 }
